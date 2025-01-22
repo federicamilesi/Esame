23288c7592a479250f27b440204c77d3e12e43cf
Federica Milesi <fede.arctic@gmail.com>
2025-01-22 09:09:41 +0100

    finestre ok

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const openWindowButton = document.getElementById("openWindow");
     openWindowButton.addEventListener('click', () => {
+    const width = 600;
+    const height = 400;
+    const x = Math.floor(Math.random() * (window.innerWidth - width));
+    const y = Math.floor(Math.random() * (window.innerHeight - height));
         new WinBox({
           title: "Brescia Calcio",
+          x: x,
+          y: y,
           url: "prog1.html",
           width: "600px",
           height: "400px",
-          onfocus: () => console.log("La finestra è attiva."),
-          onclose: () => console.log("Hai chiuso la finestra.")
+          border: "10px",
+          background: "#202020",
         });
       });
     });
