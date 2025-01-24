7dc7823e81488ce0220f8ff4cd9fa73cb20b15d3
Federica Milesi <fede.arctic@gmail.com>
2025-01-24 11:48:32 +0100

    Prova scorrimetno cartelle

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const openWindowButton = document.getElementById("openWindow");
-    openWindowButton.addEventListener('click', () => {
-    const width = 600;
-    const height = 400;
-    const x = Math.floor(Math.random() * (window.innerWidth - width));
-    const y = Math.floor(Math.random() * (window.innerHeight - height));
-        new WinBox({
-          title: "Brescia Calcio",
-          x: x,
-          y: y,
-          url: "prog1.html",
-          width: "600px",
-          height: "400px",
-          border: "10px",
-          background: "#202020",
-        });
+  console.log('DOMloaded');
+  const buttons = document.querySelectorAll('.openWindow');
+
+
+  buttons.forEach(button => {
+      button.addEventListener('click', () => {
+          const url = button.getAttribute('data-url');
+          const width = 600;
+          const height = 400;
+          const x = Math.floor(Math.random () * (window.innerWidth - width));
+          const y = Math.floor(Math. random () * (window.innerHeight - height));
+          new WinBox({
+              x: x,
+              y: y,
+              title: "I miei Progetti",
+              url: url, 
+              width: "600px",
+              height: "400px",
+              border: "0.3em",
+              background: "#202020"
+          });
       });
-    });
+  });
+});
