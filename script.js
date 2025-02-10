bc8aec36839de72e9b192dbcc26ab377b3ed4845
Federica Milesi <fede.arctic@gmail.com>
2025-02-10 11:08:33 +0100

    FINE

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
       button.addEventListener('click', () => {
           const url = button.getAttribute('data-url');
           const width = Math.floor(window.innerWidth * 0.6); 
-          const height = Math.floor(window.innerHeight * 0.5);
+          const height = Math.floor(window.innerHeight * 0.7);
           const x = Math.floor(Math.random () * (window.innerWidth - width));
           const y = Math.floor(Math. random () * (window.innerHeight - height));
           new WinBox({
